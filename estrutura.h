6dd4f1be8740059a824bc25c24ac00fad1effc61
luanvalese <51764124+LuanValese@users.noreply.github.com>
2019-12-08 15:34:28 -0300

    Add files via upload

---
+++ estrutura.h
@@ -0,0 +1,13 @@
+//#ifdef ESTRUTURA_H
+//#define ESTRUTURA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct no{
+    
+    char palavra[100];
+    char palavraOrdenada[100];
+}No;
+
+//#endif
